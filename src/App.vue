9e8e899c92b70da4a3793b8fb4ced26c09998e40
tabatamasahiro <tbtmshr2@gmail.com>
2019-07-15 11:56:00 +0900

    udemy 勉強中

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>
+          マイアドレス帳
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>Link One</v-btn>
+          <v-btn flat>Link Two</v-btn>
+          <v-btn flat>Link Three</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+
+      <SideNav/>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import SideNav from './components/SideNav'
+
+export default {
+  name: 'App',
+  components: {
+    SideNav
+  },
+  data () {
+    return {
+      //
     }
   }
 }
-</style>
+</script>
--- src/router.js
+++ src/router.js
@@ -4,14 +4,20 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
+/**
+ * ルーティングの設定
+ */
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  mode: 'history',            //パスをhistoryで保持
+  base: process.env.BASE_URL, //デフォルトは [/] から始まるパス。
+  /**
+   * 書くページへのルートを設定
+   */
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/',    
+      name: 'home',   // 名前で遷移先を指定できる
+      component: Home // 上で読み込んだコンポーネント
     },
     {
       path: '/about',
@@ -19,6 +25,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      //
+      // 引数のコメントは、コメントじゃないよ。webppackの設定だよ。
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
